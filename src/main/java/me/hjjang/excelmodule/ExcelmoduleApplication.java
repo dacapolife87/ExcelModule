779d030793c2717636eca960ce86c10f6cc928b3
HyungjuJang <dacapolife87@gmail.com>
2021-02-03 16:05:47 +0900

    Initial commit

---
+++ src/main/java/me/hjjang/excelmodule/ExcelmoduleApplication.java
@@ -0,0 +1,13 @@
+package me.hjjang.excelmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelmoduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hjjang/excelmodule/ExcelmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hjjang.excelmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
