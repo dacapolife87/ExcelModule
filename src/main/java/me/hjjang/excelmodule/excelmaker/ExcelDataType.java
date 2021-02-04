dfe421c93b6b302d72d88651b12908fb1793b0b9
HyungjuJang <dacapolife87@gmail.com>
2021-02-04 12:44:10 +0900

    refactor : Cell Type별 Enum으로 처리 및 스타일 처리

---
+++ src/main/java/me/hjjang/excelmodule/excelmaker/ExcelDataType.java
@@ -0,0 +1,44 @@
+package me.hjjang.excelmodule.excelmaker;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public enum ExcelDataType {
+    DOUBLE(Double.TYPE, 0, Arrays.asList(int.class, Integer.class,long.class, Long.class)),
+    BOOLEAN(Boolean.TYPE, 0, Arrays.asList(boolean.class, Boolean.class)),
+    LOCALDATE(LocalDate.class, 14, Arrays.asList(LocalDate.class)),
+    LOCALDATETIME(LocalDateTime.class, 22, Arrays.asList(LocalDateTime.class)),
+    CALENDAR(Calendar.class, 22, Arrays.asList(Calendar.class)),
+    DATE(Date.class, 14, Arrays.asList(Date.class)),
+    STRING(String.class,0,Collections.EMPTY_LIST);
+
+    private Class type;
+    private int style;
+    private List typeClasses;
+
+    ExcelDataType(Class type, int style, List typeClasses) {
+        this.type = type;
+        this.style = style;
+        this.typeClasses = typeClasses;
+    }
+
+    public Class value() {
+        return type;
+    }
+
+    public int style() {
+        return style;
+    }
+    public static ExcelDataType findByClassType(Class clazz){
+        return Arrays.stream(ExcelDataType.values())
+                        .filter(typeGroup -> typeGroup.hasClassType(clazz))
+                        .findAny()
+                        .orElse(STRING);
+    }
+
+    public boolean hasClassType(Class clazz){
+        return typeClasses.stream()
+                .anyMatch(classType -> classType.equals(clazz));
+    }
+}
--- src/main/java/me/hjjang/excelmodule/excelmaker/ExcelHandler.java
+++ src/main/java/me/hjjang/excelmodule/excelmaker/ExcelHandler.java
@@ -2,6 +2,8 @@ package me.hjjang.excelmodule.excelmaker;
 
 import me.hjjang.excelmodule.annotation.ExcelCellMapping;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -10,58 +12,55 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class ExcelHandler<T> {
 
     private static final String XLS_EXTENSION = ".xlsx";
+    private static final String METHOD_SET_CELL_VALUE = "setCellValue";
 
-    public void excelMaker(String fileName,List<T> dataList, Class clazz) throws IllegalAccessException, NoSuchFieldException, IOException {
+    public void excelMaker(String fileName,List<T> dataList, Class clazz) throws IllegalAccessException, NoSuchFieldException, IOException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         File file = new File(fileName + XLS_EXTENSION);
         try (FileOutputStream fos = new FileOutputStream(file)) {
-
             XSSFWorkbook newWorkBook = new XSSFWorkbook();
-            XSSFSheet newSheet = newWorkBook.createSheet();
+
             Map<Integer, String> headerIndexMap = new HashMap<>();
             List<String> headers = new ArrayList<>();
             Field[] dtoFields = clazz.getDeclaredFields();
             
             getTargetFieldForWriteExcel(dtoFields, headers, headerIndexMap);
-            inputHeader(newSheet, headers);
-            writeCell(dataList, newSheet, headers, headerIndexMap, clazz);
+            writeCell(dataList, newWorkBook, headers, headerIndexMap, clazz);
             newWorkBook.write(fos);
         }
 
     }
 
-    private void writeCell(List<T> dataList, XSSFSheet newSheet, List<String> headers, Map<Integer, String> headerIndexMap, Class clazz) throws IllegalAccessException, NoSuchFieldException {
+    private void writeCell(List<T> dataList, XSSFWorkbook workbook, List<String> headers, Map<Integer, String> headerIndexMap, Class clazz) throws IllegalAccessException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
+        XSSFSheet newSheet = workbook.createSheet();
+        inputHeader(newSheet, headers);
+
         for (int i = 0; i < dataList.size(); i++) {
             XSSFRow bodyRow = newSheet.createRow(i + 1);
+
             T object = dataList.get(i);
             for (int j = 0; j < headers.size(); j++) {
                 Cell cell = bodyRow.createCell(j);
-
                 String fieldName = headerIndexMap.get(j);
 
                 Field field = clazz.getDeclaredField(fieldName);
                 field.setAccessible(true);
                 Class<?> fieldType = field.getType();
 
-                if (fieldType == LocalDateTime.class) {
-                    String date = ((LocalDateTime) field.get(object)).format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm"));
-                    cell.setCellValue(date);
-                } else if (fieldType == long.class || fieldType == Long.class) {
-                    cell.setCellValue((Long) field.get(object));
-                } else if (fieldType == int.class || fieldType == Integer.class) {
-                    cell.setCellValue((Integer) field.get(object));
-                } else if (fieldType == boolean.class || fieldType == Boolean.class) {
-                    cell.setCellValue((Boolean) field.get(object));
-                } else {
-                    cell.setCellValue((String) field.get(object));
-                }
+                ExcelDataType dataTypeClazz = ExcelDataType.findByClassType(fieldType);
 
+                Method setCellValue = cell.getClass().getMethod(METHOD_SET_CELL_VALUE, dataTypeClazz.value());
+                setCellValue.invoke(cell, field.get(object));
+
+                XSSFCellStyle cellStyle = workbook.createCellStyle();
+                cellStyle.setDataFormat(dataTypeClazz.style());
+                cell.setCellStyle(cellStyle);
             }
         }
     }
