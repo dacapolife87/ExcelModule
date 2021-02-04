0c6252e61947763c0d3367a48332615329c5d394
HyungjuJang <dacapolife87@gmail.com>
2021-02-04 15:10:24 +0900

    refactor : 데이터 일급컬렉션

---
+++ src/main/java/me/hjjang/excelmodule/excelmaker/ExcelSheetData.java
@@ -0,0 +1,20 @@
+package me.hjjang.excelmodule.excelmaker;
+
+import java.util.List;
+
+public class ExcelSheetData<T> {
+
+    private List<T> dataList;
+
+    public ExcelSheetData(List<T> dataList) {
+        this.dataList = dataList;
+    }
+
+    public int dataSize() {
+        return dataList.size();
+    }
+
+    public Object data(int index) {
+        return dataList.get(index);
+    }
+}
