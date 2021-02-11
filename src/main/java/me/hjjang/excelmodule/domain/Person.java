f40bd59d15eae0fedf609e133987befccf3d07da
hJJANG <dacapolife87@gmail.com>
2021-02-11 10:13:04 +0900

    refactor : 테스트용 객체는 테스트 패키지에 있는게 맞다고 판단하여 이동

--- src/main/java/me/hjjang/excelmodule/domain/Person.java
+++ src/main/java/me/hjjang/excelmodule/domain/Person.java
@@ -1,25 +1,25 @@
-package me.hjjang.excelmodule.domain;
-
-import me.hjjang.excelmodule.annotation.ExcelCellMapping;
-
-public class Person {
-
-    public Person(String name, int age, String address, String etc) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-        this.etc = etc;
-    }
-
-    @ExcelCellMapping(name = "이름")
-    private String name;
-
-    @ExcelCellMapping(name = "나이")
-    private int age;
-
-    @ExcelCellMapping(name = "주소")
-    private String address;
-
-    private String etc;
-
-}
+//package me.hjjang.excelmodule.domain;
+//
+//import me.hjjang.excelmodule.annotation.ExcelCellMapping;
+//
+//public class Person {
+//
+//    public Person(String name, int age, String address, String etc) {
+//        this.name = name;
+//        this.age = age;
+//        this.address = address;
+//        this.etc = etc;
+//    }
+//
+//    @ExcelCellMapping(name = "이름")
+//    private String name;
+//
+//    @ExcelCellMapping(name = "나이")
+//    private int age;
+//
+//    @ExcelCellMapping(name = "주소")
+//    private String address;
+//
+//    private String etc;
+//
+//}
---
+++ src/test/java/me/hjjang/excelmodule/domain/ObjectForTest.java
@@ -0,0 +1,40 @@
+package me.hjjang.excelmodule.domain;
+
+import me.hjjang.excelmodule.annotation.ExcelCellMapping;
+import me.hjjang.excelmodule.excelmaker.enumtype.ExcelCellType;
+
+import java.time.LocalDateTime;
+
+public class ObjectForTest {
+
+    @ExcelCellMapping(name = "WrapperLong")
+    private Long longTypeWrap;
+
+    @ExcelCellMapping(name = "PrimitiveLong")
+    private long longTypePrimitive;
+
+    @ExcelCellMapping(name = "StringType")
+    private String stringType;
+
+    @ExcelCellMapping(name = "문자열")
+    private String stringTypeHeaderKorean;
+
+    @ExcelCellMapping(name = "WrapperInt", type = ExcelCellType.STRING)
+    private Integer intTypeWrap;
+
+    @ExcelCellMapping(name = "PrimitiveInt", type = ExcelCellType.STRING)
+    private int intTypePrimitive;
+
+    @ExcelCellMapping(name = "LocalDateTime", type = ExcelCellType.DATETIME)
+    private LocalDateTime localDateTime;
+
+    public ObjectForTest(Long longTypeWrap, long longTypePrimitive, String stringType, String stringTypeHeaderKorean, Integer intTypeWrap, int intTypePrimitive, LocalDateTime localDateTime) {
+        this.longTypeWrap = longTypeWrap;
+        this.longTypePrimitive = longTypePrimitive;
+        this.stringType = stringType;
+        this.stringTypeHeaderKorean = stringTypeHeaderKorean;
+        this.intTypeWrap = intTypeWrap;
+        this.intTypePrimitive = intTypePrimitive;
+        this.localDateTime = localDateTime;
+    }
+}
